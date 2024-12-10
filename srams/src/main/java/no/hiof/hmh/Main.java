4dcb308e8c08c070f45d50700d0e5faa1e9fb410
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2024-12-10 10:15:26 +0100

    Forced faileur

--- srams/src/main/java/no/hiof/hmh/Main.java
+++ srams/src/main/java/no/hiof/hmh/Main.java
@@ -2,7 +2,7 @@ package no.hiof.hmh;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello worl!");
 
         //comment ddd
 
