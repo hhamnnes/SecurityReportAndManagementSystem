ac8790e4621f7bb28ec9aa0323975cab7ebc38b3
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2024-12-10 15:28:27 +0100

    SecurityOccurence class is created based on tests SecurityOccurence class has been created based on alredy created tests

---
+++ srams/src/main/java/no/hiof/hmh/domain/model/SecurityOccurrenceCategory.java
@@ -0,0 +1,11 @@
+package no.hiof.hmh.domain.model;
+
+public interface SecurityOccurrenceCategory {
+
+    String getType();
+    
+    String getName();
+
+    String getDescription();
+
+}
--- srams/src/test/java/no/hiof/hmh/domain/model/SecurityOccurrenceTest.java
+++ srams/src/test/java/no/hiof/hmh/domain/model/SecurityOccurrenceTest.java
@@ -1,8 +1,7 @@
 package no.hiof.hmh.domain.model;
 
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.time.LocalDateTime;
@@ -18,9 +17,6 @@ public class SecurityOccurrenceTest {
 
     @Mock
     private SecurityOccurrenceCategory mockSecurityOccurrenceCategory;
-
-    @Mock
-    private LocalDateTime mockLocalDateTime;
 
     @Test
     @DisplayName("Test the default constructor")
@@ -40,32 +36,52 @@ public class SecurityOccurrenceTest {
             String name = "Some name";
             String description = "Some description";
             String location = "Some location";
+            LocalDateTime timestamp = LocalDateTime.now();
 
-            when(mockSecurityOccurrenceCategory.getName()).thenReturn("Some category name");
-            when(mockLocalDateTime.toString()).thenReturn("2023-12-12T12:12:12");
+            when(mockSecurityOccurrenceCategory.getType()).thenReturn("Some category name");
 
             //Act 
             SecurityOccurrence securityOccurrence = new SecurityOccurrence(name,
                                                                         description, 
                                                                         location,
                                                                         mockSecurityOccurrenceCategory,
-                                                                        mockLocalDateTime);
+                                                                        timestamp);
 
             //Assert
-            String expectedTime = "2023-12-12T12:12:12";
             assertAll(
                 () -> assertNotNull(securityOccurrence, "Should exist, therefore not null"),
                 () -> assertEquals(description, securityOccurrence.getDescription(), "Should match"),
                 () -> assertEquals(location, securityOccurrence.getLocation(), "Should match"),
-                () -> assertEquals(expectedTime, securityOccurrence.getTimestamp(),
+                () -> assertEquals(timestamp, securityOccurrence.getTimestamp(),
                  "The time should be 2023-12-12T12:12:12"),
                 () -> assertEquals(mockSecurityOccurrenceCategory, securityOccurrence.getCategory(), "Should be the expected mocked version"
-                + "of SecurityOccurrenceCategory")
+                + "of SecurityOccurrenceCategory"),
+                () -> assertEquals(mockSecurityOccurrenceCategory.getType(), securityOccurrence.getCategory().getType())
             );
         } catch (Exception e) {
             System.out.println(e);
         }
         
+
+
+    }
+
+
+    @Test
+    @DisplayName("Test with null values for timestamp and category parameters")
+    public void testConstructorWithNullParameters() {
+        
+        //Arrange
+        String name = "Some name";
+        String description = "Some description";
+        String location = "Some location";
+
+        //Act and assert
+        assertThrows(IllegalArgumentException.class, () -> {
+            new SecurityOccurrence(name, description, location, null, null);
+        });
+
+
 
 
     }
